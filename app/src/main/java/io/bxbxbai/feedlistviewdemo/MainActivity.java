5d994b7db4bb03d4b645344bdc94418337c10297
bxbxbai <bxbxbai@gmail.com>
2014-09-19 20:07:13 +0800

    action bar title use ShimmerTextView

--- app/src/main/java/io/bxbxbai/feedlistviewdemo/MainActivity.java
+++ app/src/main/java/io/bxbxbai/feedlistviewdemo/MainActivity.java
@@ -1,16 +1,21 @@
 package io.bxbxbai.feedlistviewdemo;
 
 import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.romainpiel.shimmer.Shimmer;
+import com.romainpiel.shimmer.ShimmerTextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -32,12 +37,15 @@ public class MainActivity extends Activity {
     private List<FeedItem> feedItems;
     private String URL_FEED = "http://api.androidhive.info/feed/feed.json";
 
+    private ActionBar mActionbar;
+    private ShimmerTextView mActionbarTitle;
+
     @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        initActionbar();
         listView = (ListView) findViewById(R.id.feed_list);
 
         feedItems = new ArrayList<FeedItem>();
@@ -107,6 +115,22 @@ public class MainActivity extends Activity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    private void initActionbar() {
+        mActionbar = getActionBar();
+        mActionbar.setDisplayHomeAsUpEnabled(true);
+        mActionbar.setDisplayShowHomeEnabled(false);
+        mActionbar.setHomeButtonEnabled(true);
+        mActionbar.setIcon(R.drawable.ic_launcher);
+        mActionbar.setDisplayShowTitleEnabled(false);
+        mActionbar.setDisplayShowCustomEnabled(true);
+
+        View view = View.inflate(this, R.layout.layout_actionbar, null);
+        mActionbarTitle = (ShimmerTextView) view.findViewById(R.id.tv_action_bar_title);
+        new Shimmer().start(mActionbarTitle);
+        mActionbarTitle.setText("FeedListViewDemo");
+        mActionbar.setCustomView(view);
     }
 
     @Override
